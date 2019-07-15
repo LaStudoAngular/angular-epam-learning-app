c8c930768cf57f1458e138e75b751b5014c8b467
Moe Green <gearmobile@gmail.com>
2019-07-15 18:20:10 +0300

    feat: create components, interfaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CoursesComponent } from './components/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoursesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
