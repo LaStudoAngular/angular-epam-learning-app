2adbac037bab3e5420f86ab1ff4fb68a0460e2dc
Valery Semenenko <gearmobile@gmail.com>
2019-07-28 16:52:09 +0300

    feat: replace link by span in more part of course component

--- src/app/components/courses/courses-list/courses-list-item/courses-list-item.component.ts
+++ src/app/components/courses/courses-list/courses-list-item/courses-list-item.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ep-courses-list-item',
   templateUrl: './courses-list-item.component.html',
-  styleUrls: ['./courses-list-item.component.scss']
+  styleUrls: ['./courses-list-item.component.scss'],
 })
 export class CoursesListItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
