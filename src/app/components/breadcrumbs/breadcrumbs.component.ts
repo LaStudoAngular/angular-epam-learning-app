3dd5e5e42e574510a3fe67eb1524c04a3b1d9b6f
Valery Semenenko <gearmobile@gmail.com>
2019-07-28 16:59:34 +0300

    feat: add :host and hostbinding support for breadcrumbs component

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ep-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.scss']
+  styleUrls: ['./breadcrumbs.component.scss'],
 })
 export class BreadcrumbsComponent implements OnInit {
+  @HostBinding() readonly class = 'container';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
