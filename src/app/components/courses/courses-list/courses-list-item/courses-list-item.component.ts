0a617d587f865e1d00dd2baf7a02a0cd8dd52847
Valery Semenenko <gearmobile@gmail.com>
2019-07-29 18:58:42 +0300

    feat: add mockup data for output in course item component

--- src/app/components/courses/courses-list/courses-list-item/courses-list-item.component.ts
+++ src/app/components/courses/courses-list/courses-list-item/courses-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../../../../@interfaces/course';
 
 @Component({
   selector: 'ep-courses-list-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list-item.component.scss'],
 })
 export class CoursesListItemComponent implements OnInit {
+  @Input() course: Course;
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/components/courses/courses-list/courses-list.component.ts
+++ src/app/components/courses/courses-list/courses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../../../@interfaces/course';
 
 @Component({
   selector: 'ep-courses-list',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list.component.scss']
 })
 export class CoursesListComponent implements OnInit {
+  courses: Course[];
 
   constructor() { }
 
   ngOnInit() {
+    this.courses = [
+      {
+        id: 1,
+        title: 'video course 1. name tag',
+        creation_date: '9 Nov, 2018',
+        duration: '1h 28 min',
+        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' },
+      {
+        id: 2,
+        title: 'video course 2. name tag',
+        creation_date: '9 Nov, 2018',
+        duration: '1h 28 min',
+        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' },
+      {
+        id: 3,
+        title: 'video course 3. name tag',
+        creation_date: '9 Nov, 2018',
+        duration: '1h 28 min',
+        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' }
+    ];
+  }
+
+  trackByFn(index, item) {
+    return item ? item.id : undefined;
   }
 
 }
