969b010cefe42e321362138f66a4d43ae0908455
Valery Semenenko <gearmobile@gmail.com>
2019-07-29 20:58:24 +0300

    feat: add sticky footer support

--- src/app/components/courses/courses-list/courses-list.component.ts
+++ src/app/components/courses/courses-list/courses-list.component.ts
@@ -18,19 +18,22 @@ export class CoursesListComponent implements OnInit {
         title: 'video course 1. name tag',
         creation_date: '9 Nov, 2018',
         duration: '1h 28 min',
-        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description.Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' },
+        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description.Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
+      },
       {
         id: 2,
         title: 'video course 2. name tag',
-        creation_date: '9 Nov, 2018',
-        duration: '1h 28 min',
-        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' },
-      {
-        id: 3,
-        title: 'video course 3. name tag',
-        creation_date: '9 Nov, 2018',
-        duration: '1h 28 min',
-        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.' }
+        creation_date: '10 Nov, 2018',
+        duration: '2h 28 min',
+        description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
+      },
+      // {
+      //   id: 3,
+      //   title: 'video course 3. name tag',
+      //   creation_date: '11 Nov, 2018',
+      //   duration: '3h 28 min',
+      //   description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
+      // }
     ];
   }
 
