8a4696d01810f481e46a1655811eec1ea8349b63
Moe Green <gearmobile@gmail.com>
2019-09-19 21:28:06 +0300

    feat: add unsubscribe

--- src/app/@auth/auth.guard.ts
+++ src/app/@auth/auth.guard.ts
@@ -1,22 +1,39 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 import { AuthService } from '../@services/auth.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+export class AuthGuard implements CanActivate, OnDestroy {
+  constructor(private authService: AuthService, private router: Router) {}
   public result: boolean;
+  private destroy = new Subject();
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    this.authService.isAuth$.subscribe((response: boolean) => {
-      this.result = response;
-    });
-    return this.result;
+    this.authService.isAuth$
+      .pipe(takeUntil(this.destroy))
+      .subscribe((response: boolean) => (this.result = response));
+    if (this.result) {
+      return this.result;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
