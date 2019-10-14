d347fec1cd479225886b2c5ee66a8f5f9ab066f3
Valery Semenenko <gearmobile@gmail.com>
2019-10-14 22:12:14 +0300

    feat: add selector with parameter

--- src/app/components/courses/course-edit-item/course-edit-item.component.ts
+++ src/app/components/courses/course-edit-item/course-edit-item.component.ts
@@ -7,6 +7,9 @@ import { delay, takeUntil } from 'rxjs/operators';
 import { Course } from '../../../@models/course';
 import { Author } from '../../../@models/author';
 import uuid from 'uuid/v1';
+import { Store, select } from '@ngrx/store';
+import { ICourseStates } from 'src/app/store/state/course.states';
+import { selectSelectedCourse } from 'src/app/store/selectors/course.selectors';
 
 @Component({
   selector: 'ep-course-edit-item',
@@ -20,6 +23,7 @@ export class CourseEditItemComponent implements OnInit, OnDestroy {
     private courseService: CourseService,
     private router: Router,
     private route: ActivatedRoute,
+    private store: Store<ICourseStates>
   ) {}
   private destroyedSource: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
   public form: FormGroup;
@@ -46,10 +50,10 @@ export class CourseEditItemComponent implements OnInit, OnDestroy {
     // GET ID OF SELECTED COURSE
     this.route.params.subscribe((data: { id: string }) => {
       const courseID = Number(data.id);
-
-      // GET SELECTED COURSE FROM LOCAL DATABASE
-      this.courseService.getSelectedCourse(courseID).subscribe((course: Course) => {
+      // GET SELECTED COURSE FROM STORE
+      this.store.pipe(select(selectSelectedCourse(courseID))).subscribe((course: Course) => {
         this.course = course;
+        // PATCH FORM FIELDS
         this.form.patchValue({
           title: this.course.name,
           date: this.formatDate(course.date),
@@ -66,6 +70,7 @@ export class CourseEditItemComponent implements OnInit, OnDestroy {
         // GENERATE BREADCRUMBS FROM SELECTED COURSE
         this.courseService.title$.next(`${course.name}`);
       });
+
     });
   }
 
--- src/app/store/selectors/course.selectors.ts
+++ src/app/store/selectors/course.selectors.ts
@@ -1,5 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ICourseStates } from '../state/course.states';
+import { Course } from 'src/app/@models/course';
 
 export const selectCourseFeature = createFeatureSelector<ICourseStates>('courses');
 
@@ -7,3 +8,8 @@ export const selectCourses = createSelector(
   selectCourseFeature,
   (state: ICourseStates) => state.courses
 );
+
+export const selectSelectedCourse = (id: number) => createSelector(
+  selectCourseFeature,
+  (state: ICourseStates) => state.courses.filter((el: Course) => el.id === id)[0]
+)
