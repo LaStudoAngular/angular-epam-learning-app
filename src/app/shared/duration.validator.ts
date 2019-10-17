2b37461990f7547ea45b450155e8447ed6a2186a
Valery Semenenko <gearmobile@gmail.com>
2019-10-17 23:03:51 +0300

    feat: add custom validation function

--- src/app/shared/duration.validator.ts
+++ src/app/shared/duration.validator.ts
@@ -2,5 +2,5 @@ import { AbstractControl } from '@angular/forms';
 
 export function validateDuration(control: AbstractControl): {[key: string]: { [key: string]: string }} | null {
   const testControl = /^[0-9]*$/.test(control.value);
-  return testControl ? null : { errorMessage: { value: 'Only numbers allowed' } }
+  return testControl ? null : { errorMessage: { value: 'input numbers error' } }
 }
