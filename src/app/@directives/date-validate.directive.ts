c7a62ea166449fcc94959af4f1a7d1740db7a963
Valery Semenenko <gearmobile@gmail.com>
2019-10-18 14:17:35 +0300

    feat: fix template driven validation

--- src/app/@directives/date-validate.directive.ts
+++ src/app/@directives/date-validate.directive.ts
@@ -2,23 +2,23 @@ import { Directive } from '@angular/core';
 import { NG_VALIDATORS, FormControl } from '@angular/forms';
 
 @Directive({
-  selector: '[epDateValidate][formControlName]',
+  selector: '[epDateValidate]',
   providers: [
     {
       provide: NG_VALIDATORS,
-      useValue: DateValidateDirective,
+      useValue: ptDateValidate,
       multi: true
     }
   ]
 })
-export class DateValidateDirective {
-  ptDate(control: FormControl): { [key: string]: any } | any {
-    const ptDatePattern = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
-    return control.value.match(ptDatePattern) ? null : { dateError: { value: 'date input error' } }
-  }
+export class DateValidateDirective {}
 
-  usDate(control: FormControl): { [key: string]: any } | any {
-    const usDatePattern = /^02\/(?:[01]\d|2\d)\/(?:19|20)(?:0[048]|[13579][26]|[2468][048])|(?:0[13578]|10|12)\/(?:[0-2]\d|3[01])\/(?:19|20)\d{2}|(?:0[469]|11)\/(?:[0-2]\d|30)\/(?:19|20)\d{2}|02\/(?:[0-1]\d|2[0-8])\/(?:19|20)\d{2}$/;
-    return control.value.match(usDatePattern) ? null : { dateError: { value: 'date input error' } }
-  }
+function ptDateValidate(control: FormControl): { [key: string]: any } | null {
+  const ptDatePattern = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+  return control.value.match(ptDatePattern) ? null : { errorDate: { value: 'date input error' } }
+}
+
+function usDateValidate(control: FormControl): { [key: string]: any } | null {
+  const usDatePattern = /^02\/(?:[01]\d|2\d)\/(?:19|20)(?:0[048]|[13579][26]|[2468][048])|(?:0[13578]|10|12)\/(?:[0-2]\d|3[01])\/(?:19|20)\d{2}|(?:0[469]|11)\/(?:[0-2]\d|30)\/(?:19|20)\d{2}|02\/(?:[0-1]\d|2[0-8])\/(?:19|20)\d{2}$/;
+  return control.value.match(usDatePattern) ? null : { errorDate: { value: 'date input error' } }
 }
