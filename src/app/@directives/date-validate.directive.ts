c7416c763ad3597f113fe7613f473cb4ae2e45b4
Valery Semenenko <gearmobile@gmail.com>
2019-10-18 11:42:34 +0300

    feat: add date validator

--- src/app/@directives/date-validate.directive.ts
+++ src/app/@directives/date-validate.directive.ts
@@ -1,20 +1,24 @@
 import { Directive } from '@angular/core';
-import { NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { NG_VALIDATORS, FormControl } from '@angular/forms';
 
 @Directive({
-  selector: '[epDateValidate]',
+  selector: '[epDateValidate][formControlName]',
   providers: [
-    provide: NG_VALIDATORS,
-    useValue: dateValidate,
+    {
+      provide: NG_VALIDATORS,
+      useValue: DateValidateDirective,
+      multi: true
+    }
   ]
 })
 export class DateValidateDirective {
+  ptDate(control: FormControl): { [key: string]: any } | any {
+    const ptDatePattern = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+    return control.value.match(ptDatePattern) ? null : { dateError: { value: 'date input error' } }
+  }
 
-  constructor() { }
-
-}
-
-function dateValidate(control: AbstractControl) {
-  const pattern = /(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/;
-
+  usDate(control: FormControl): { [key: string]: any } | any {
+    const usDatePattern = /^02\/(?:[01]\d|2\d)\/(?:19|20)(?:0[048]|[13579][26]|[2468][048])|(?:0[13578]|10|12)\/(?:[0-2]\d|3[01])\/(?:19|20)\d{2}|(?:0[469]|11)\/(?:[0-2]\d|30)\/(?:19|20)\d{2}|02\/(?:[0-1]\d|2[0-8])\/(?:19|20)\d{2}$/;
+    return control.value.match(usDatePattern) ? null : { dateError: { value: 'date input error' } }
+  }
 }
